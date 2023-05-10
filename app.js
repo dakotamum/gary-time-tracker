972d389bd218d61c88c502e33dc9ee5b5e92fb29
Dakota Mumford <dakotamum@gmail.com>
2023-05-09 21:37:25 -0600

    Co-authored-by: Brennon Netz <bnetz15@gmail.com>

---
+++ app.js
@@ -0,0 +1,10 @@
+function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+function getKeyString(x, y) {
+    return `${x}x${y}`;
+}
+
+(function () {
+    firebase.auth().signInAnonymously();
+})();
